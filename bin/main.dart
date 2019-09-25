7ec4c5f99cc4e9ba3b405736730733eed97197bf
piedcipher <pateltirth454@gmail.com>
2019-09-25 23:26:21 +0530

    Added argument parser which offers options to view, online/all friends & help

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,25 @@
+import 'dart:io';
+
 import 'package:jojo/jojo.dart';
+import 'package:args/args.dart';
 
 void main(final List<String> arguments) {
-  JoJo(option: 'of');
+  final ArgParser argParser = ArgParser();
+  argParser.addFlag('onlinefriends',
+      abbr: 'o', help: 'Shows Online Friends', negatable: false);
+  argParser.addFlag('friends',
+      abbr: 'f', help: 'Shows Friends', negatable: false);
+  argParser.addFlag('help', abbr: 'h', help: 'Help', negatable: false);
+  try {
+    final ArgResults argResults = argParser.parse(arguments);
+    argResults.arguments.isNotEmpty
+        ? argResults.arguments[0] != '-h'
+            ? JoJo(option: argResults.arguments[0])
+            : stdout.writeln(
+                '\nJoJo is a CLI utility to check last-visit of your CodeForces friends & much more.\n\n${argParser.usage}\n')
+        : stderr.writeln('\nNo Flags Supplied. Use -h to print help.\n');
+  } on FormatException {
+    stderr.writeln('\nInvalid Flag. Available flags are listed below\n');
+    stdout.writeln('\n${argParser.usage}\n');
+  }
 }
--- lib/jojo.dart
+++ lib/jojo.dart
@@ -11,22 +11,23 @@ const String API_BASE_URL = 'https://codeforces.com/api/';
 class JoJo {
   JoJo({final String option}) {
     switch (option) {
-      case "of":
-        _getOnlineFriends();
+      case '-o':
+        _getFriends(onlyOnline: true);
+        break;
+      case '-f':
+        _getFriends();
         break;
     }
   }
 
-  void _getOnlineFriends() async {
+  void _getFriends({bool onlyOnline = false}) async {
     final http.Response response = await http.get(
-        '${API_BASE_URL}user.friends?onlyOnline=true&apiKey=${API_KEY}&time=${DateTime.now().millisecondsSinceEpoch ~/ 1000}&apiSig=123456${sha512.convert(utf8.encode("123456/user.friends?apiKey=${API_KEY}&onlyOnline=true&time=${DateTime.now().millisecondsSinceEpoch ~/ 1000}#${API_SECRET}"))}');
+        '${API_BASE_URL}user.friends?onlyOnline=${onlyOnline}&apiKey=${API_KEY}&time=${DateTime.now().millisecondsSinceEpoch ~/ 1000}&apiSig=123456${sha512.convert(utf8.encode("123456/user.friends?apiKey=${API_KEY}&onlyOnline=${onlyOnline}&time=${DateTime.now().millisecondsSinceEpoch ~/ 1000}#${API_SECRET}"))}');
     final Map<String, dynamic> parsedResponse = jsonDecode(response.body);
     const String PROFILE_BASE_URL = 'https://codeforces.com/profile/';
-    if (parsedResponse['status'] == 'OK') {
-      (parsedResponse['result'] as List)
-          .forEach((final onlineFriend) => stdout.writeln('$onlineFriend - $PROFILE_BASE_URL$onlineFriend'));
-    } else {
-      stderr.writeln(parsedResponse['comment']);
-    }
+    parsedResponse['status'] == 'OK'
+        ? (parsedResponse['result'] as List).forEach((final onlineFriend) =>
+            stdout.writeln('$onlineFriend - $PROFILE_BASE_URL$onlineFriend'))
+        : stderr.writeln('\n${parsedResponse['comment']}\n');
   }
 }
